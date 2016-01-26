a3c903b33025c7b21f85aa11ee8d14ffb509127c
sxf <sun_xfancy@163.com>
2016-01-27 07:50:20 +0800

    修改部分显示

--- src/Pages/Show/ShowList.tsx
+++ src/Pages/Show/ShowList.tsx
@@ -11,8 +11,12 @@ export class ShowList extends React.Component<any, ShowListState> {
     constructor(props) {
         super(props)
         this.addItem = this.addItem.bind(this)
-        this.state = { items: new Array() } as ShowListState
+        this.state = {items: this.props.data} as ShowListState
     }
+
+    private static defaultProps = {
+        data: []
+    };
 
     addItem(item) {
         this.state.items.unshift(item)
