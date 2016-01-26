22c48a1a123277cc25ba2906c62e021f4532574b
sxf <sun_xfancy@163.com>
2016-01-26 10:38:00 +0800

    change some loader objects

--- src/Pages/Class/jsonLoader.ts
+++ src/Pages/Class/jsonLoader.ts
@@ -1,13 +1,14 @@
 "use strict";
 const fs = require("fs");
 
-import {GotoMap} from './gotoMap';
-import {BnfList} from './bnfList';
-
+import {GotoMap} from './gotoMap'
+import {BnfList} from './bnfList'
+import {Vmap} from './vmap'
 
 export class JsonLoader {
     private goto_map: GotoMap;
     private bnf_list: BnfList;
+    private vmap: Vmap;
     private path: string;
 
     constructor(path: string) {
@@ -16,14 +17,13 @@ export class JsonLoader {
 
     loadAll() : void {
         var json_data = this.loadJson(this.path)
-        console.log(json_data)
         this.bnf_list = new BnfList().loadJson(json_data.bnf)
         this.goto_map = new GotoMap().loadJson(json_data.table)
+        this.vmap     = new Vmap().loadJson(json_data.vmap)
     }
 
     loadJson(path: string) {
         var data = fs.readFileSync(path, 'utf8')
-        console.log('data', data)
         var ans = null;
         try{
             ans = JSON.parse(data)
